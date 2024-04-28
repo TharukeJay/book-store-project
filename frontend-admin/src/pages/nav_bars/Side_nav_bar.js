dfa9b982eb182822dfcd5c19214ccfca7eab3779
Yasara06 <yasara06@gmail.com>
2024-04-28 19:28:25 +0530

    added login,signup and home pages II

--- frontend-admin/src/pages/nav_bars/Side_nav_bar.js
+++ frontend-admin/src/pages/nav_bars/Side_nav_bar.js
@@ -6,7 +6,7 @@ function SideNavbar() {
     return (
         <div style={{ width: '250px', background: '#f8f9fa', height: '100vh' }}>
             <Nav defaultActiveKey="/home" className="flex-column p-3">
-                <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                <Nav.Link as={Link} to="/home">Upload</Nav.Link>
                 <Nav.Link as={Link} to="/about">About</Nav.Link>
                 <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                 <Nav.Link as={Link} to="/admin">Admin Panel</Nav.Link>
